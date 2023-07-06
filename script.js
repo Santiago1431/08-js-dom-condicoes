807dba0b1ad3d14ba4397d24abb2ed44aabd8328
Aluno Biblioteca <biblioteca.aluno@ifsalinas.lan>
2023-07-06 18:41:40 -0300

    Alterando Arquivos

--- script.js
+++
@@ -1,7 +0,0 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var msg = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = 'Agora são ${hora} horas.'
-}
